e0b1aa5c1b16501dc826fef966d1b2c4150884fb
KeanuRayhan <117803802+KeanuRayhan@users.noreply.github.com>
2023-02-25 23:27:27 +0700

    Add files via upload

---
+++ Commission.java
@@ -0,0 +1,34 @@
+
+public class Commission extends Hourly{
+	private double totalSales;
+	private double commissionRate;
+	
+	public Commission(String eName, String eAddress, String ePhone, 
+			String socSecNumber, double rate, double commissionRate)
+	{
+		super(eName, eAddress, ePhone, socSecNumber, rate);
+		this.commissionRate = commissionRate;
+		totalSales = 0;
+	}
+	
+	public void addSales(double totalSales)
+	{
+		this.totalSales += totalSales;
+	}
+	
+	public double pay()
+	{
+		double payment = super.pay();
+		payment += commissionRate * totalSales;
+		totalSales = 0;
+		return payment;
+	}
+	
+	public String toString() 
+	{
+		String result = super.toString();
+		result += "\nTotal Sales: " + totalSales;
+		return result;
+	}
+
+}
